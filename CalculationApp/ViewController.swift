6eb03cc4b3bb3d2ea5ed7e8bb5ad09f468d93b13
Tea Green <4a1h0012@gmail.com>
2018-08-02 15:10:52 +0800

    Ver1.0 簡易calculator

--- CalculationApp/ViewController.swift
+++ CalculationApp/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    var sum = 0.00
+    
+    @IBAction func calculationSegmented(_ sender: UISegmentedControl) {
+        if firstNumberTextField.text != "",secondNumberTextField.text != ""{
+            Calculation(symbol: sender.selectedSegmentIndex, number1: firstNumberTextField.text!,number2: secondNumberTextField.text!)
+            answerLabel.text = "答案：\(String(format: "%0.2f", sum))"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func Calculation(symbol:Int,number1:String,number2:String){
+        
+        switch symbol {
+        case 0:
+            sum = Double(number1)!+Double(number2)!
+        case 1:
+            sum = Double(number1)!-Double(number2)!
+        case 2:
+            sum = Double(number1)!*Double(number2)!
+        case 3:
+            sum = Double(number1)!/Double(number2)!
+        default:
+            break
+        }
+        
+    }
 
 }
 
